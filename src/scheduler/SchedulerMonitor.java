b05390782b9bdba79b89ab52783ed44deacc0ba6
Nhitro <julien53laval@gmail.com>
2018-11-07 17:24:45 +0100

    Modified SchedulerMonitor

--- src/scheduler/SchedulerMonitor.java
+++ src/scheduler/SchedulerMonitor.java
@@ -1,5 +1,17 @@
 package scheduler;
 
+import java.util.concurrent.ScheduledExecutorService;
+
 public class SchedulerMonitor {
+	
+	private ScheduledExecutorService scheduler;
+	
+	public SchedulerMonitor() {
+		// on instancie
+	}
+	
+	public synchronized void schedule() {
+		// prend les mêmes param que le scheduler
+	}
 
 }
