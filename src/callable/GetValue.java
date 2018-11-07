e9f3b9e485ddee03cb626feea1a3650152c5d6bb
Nhitro <julien53laval@gmail.com>
2018-11-07 17:23:23 +0100

    Created classes

---
+++ src/callable/GetValue.java
@@ -0,0 +1,16 @@
+package callable;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class GetValue implements Callable {
+
+	@Override
+	public Object call() throws Exception {
+
+
+		
+		return null;
+	}
+
+}
---
+++ src/callable/Update.java
@@ -0,0 +1,13 @@
+package callable;
+
+import java.util.concurrent.Callable;
+
+public class Update implements Callable {
+
+	@Override
+	public Object call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
